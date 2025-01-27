cb6ee49f004f77b3e0d9111eb8ef8b008c75c931
jinyuan0425 <pdmon123@gmail.com>
2025-01-27 16:15:11 -0600

    Initialise book collection app

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,49 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.order(:title)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to(books_path, notice: 'Book was successfully created.')
+    else
+      render('new')
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to(book_path(@book), notice: 'Book was successfully updated.')
+    else
+      render('edit')
+    end
+  end
+
+  def delete
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to(books_path, notice: 'Book was successfully deleted.')
+  end
+
+  def book_params
+    params.require(:book).permit(:title)
+  end
+end
