cbacaac0fd522256339b53dd3dc124ab988a5c78
root <root@localhost>
2025-01-28 02:10:25 +0000

    Add sunny and rainy day tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -5,69 +5,13 @@ RSpec.describe Book, type: :model do
   let(:invalid_book) { Book.new(title: nil) }
 
   context 'validations' do
-    it 'is valid with a title, author, price, and published_date' do
-        book = Book.new(
-            title: 'Valid Title',
-            author: 'Valid Author',
-            price: 19.99,
-            published_date: Date.today
-        )
-        expect(book).to be_valid
+    it 'is valid with a title' do
+      expect(valid_book).to be_valid
     end
-  
+
     it 'is invalid without a title' do
-        book = Book.new(
-            title: nil,
-            author: 'Valid Author',
-            price: 19.99,
-            published_date: Date.today
-        )
-        expect(book).not_to be_valid
-        expect(book.errors[:title]).to include("can't be blank")
-    end
-
-    it 'is invalid without an author' do
-        book = Book.new(
-            title: 'Valid Title', 
-            author: nil, 
-            price: 19.99, 
-            published_date: Date.today
-        )
-        expect(book).not_to be_valid
-        expect(book.errors[:author]).to include("can't be blank")
-    end
-
-    it 'is invalid without a price' do
-        book = Book.new(
-            title: 'Valid Title', 
-            author: 'Valid Author', 
-            price: nil, 
-            published_date: Date.today
-        )
-        expect(book).not_to be_valid
-        expect(book.errors[:price]).to include("can't be blank")
-    end
-
-    it 'is invalid with a non-numeric price' do
-        book = Book.new(
-            title: 'Valid Title', 
-            author: 'Valid Author', 
-            price: 'abc', 
-            published_date: Date.today
-        )
-        expect(book).not_to be_valid
-        expect(book.errors[:price]).to include("is not a number")
-    end
-
-    it 'is invalid without a published_date' do
-        book = Book.new(
-            title: 'Valid Title', 
-            author: 'Valid Author', 
-            price: 19.99, 
-            published_date: nil
-        )
-        expect(book).not_to be_valid
-        expect(book.errors[:published_date]).to include("can't be blank")
+      expect(invalid_book).not_to be_valid
+      expect(invalid_book.errors[:title]).to include("can't be blank")
     end
   end
 end
